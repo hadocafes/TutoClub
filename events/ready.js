eb4a7be7800b3098d061848e09a3edac6c8304d8
hadocafes <107887015+hadocafes@users.noreply.github.com>
2022-06-25 15:07:32 +0200

    ¡Miro esto TutoClub!
    Le estamos enseñando a TutoClub nuestro canal de YouTube.

--- events/ready.js
+++ events/ready.js
@@ -1,7 +1,20 @@
+const { version } = require('../package.json');
+
 module.exports = {
     name: 'ready',
     once: true,
     execute(client) {
-        console.log('Listo!');
+
+        client.user.setPresence({
+            activities: [{
+                name: 'TutoDiscord',
+                type: 'WATCHING'
+            }],
+
+            status: 'idle'
+        });
+
+        console.clear()
+        console.log(`TutoClub [Versión ${version}] \n\n✅ ¡${client.user.username} está preparado para la acción!`)
     }
 }
