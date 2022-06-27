f065ad8a270618d7116168d635ec7b0b2bd21672
hadocafes <107887015+hadocafes@users.noreply.github.com>
2022-06-27 22:07:36 +0200

    Mejora en información
    Los bots que están dentro de la API tienen su propio color característico de ShelterBots + la información de los bots

--- commands/información.js
+++ commands/información.js
@@ -8,37 +8,47 @@ module.exports = {
 		const member = interaction.targetMember;
 		const userInfoEmbed = new MessageEmbed().setAuthor({ name: `Información sobre ${member.displayName}`, iconURL: user.avatarURL() });
 
+		console.log( await user.fetchFlags)
+
 		let description = `${user.flags
 			.toArray()
 			.map((flag) => flags[flag])
 			.join(' ')}`;
 		let color = 'GOLD';
 
+		userInfoEmbed
+		.addField(
+			'Usuario',
+			`**Nombre de Usuario:** ${user.username}\n**ID:** \`${user.id}\`\n**Perfil:** ${user}\n**Creado el:** <t:${Math.floor(
+				user.createdTimestamp / 1000
+			)}:F> <t:${Math.floor(user.createdTimestamp / 1000)}:R>`
+		)
+		.setThumbnail(user.avatarURL())
+		.setFooter({ text: `Se ha unido el` })
+		.setTimestamp(member.joinedTimestamp); //joinedTimestamp para DD/MM/YYYY
+
 		if (user.bot) {
 			description = '🤖' + description;
 			color = 'BLURPLE';
 
 			const data = await client.shelterClient.get(user.id);
-			if (data)
-				userInfoEmbed.addField(
-					'Información del Bot (TutoShelter)',
-					`**Dueño:** <@${data.owner}> \`${data.owner}\`\n**Prefix:** ${data.prefix}\n**Librería:** ${data.library}\n**Votos:** \`${data.votes}\``
-				);
-		}
+			
+			if (data) {
+				color = '#00a79d'
 
+				userInfoEmbed
+					.addField(`Información`, data.description)
+					.addField(
+						'Más detalles de TutoShelter',
+						`**Dueño:** <@${data.owner}> \`${data.owner}\`\n**Prefix:** ${data.prefix}\n**Librería:** ${data.library}\n**Votos:** \`${data.votes}\``
+					);
+			}
+		}
+		
 		userInfoEmbed
 			.setDescription(description)
 			.setColor(color)
-			.addField(
-				'Usuario',
-				`**Nombre de Usuario:** ${user.username}\n**ID:** \`${user.id}\`\n**Perfil:** ${user}\n**Creado el:** <t:${Math.floor(
-					user.createdTimestamp / 1000
-				)}:F> <t:${Math.floor(user.createdTimestamp / 1000)}:R>`
-			)
-			.setThumbnail(user.avatarURL())
-			.setFooter({ text: `Se ha unido el` })
-			.setTimestamp(member.joinedAt); //joinedTimestamp para DD/MM/YYYY
 		//.setImage(user.fetchBannerURL())
-		interaction.reply({ embeds: [userInfoEmbed], ephemeral: true }); //quita el efímero si quieres
+		interaction.reply({ embeds: [userInfoEmbed], ephemeral: false }); //quita el efímero si quieres
 	}
 };
