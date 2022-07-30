39ea82734f7f623848e4980edf052ff8f66b736c
hadocafes <107887015+hadocafes@users.noreply.github.com>
2022-07-30 17:34:38 +0200

    arreglo chatbot imagenes

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -10,7 +10,7 @@ module.exports = {
 
     if (interaction.channel.id === '1000691855136149554') {
 
-      console.log()
+      //return console.log(interaction.attachments)
       if (interaction.attachments) return;
 
       if (interaction.content.startsWith('\\')) return; // Ignora los mensajes que tengan "\\"
